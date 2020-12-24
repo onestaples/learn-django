31bcfc55eb40da52db5f4a88d3e4eed049e08b16
onestaples <samurisama@outlook.com>
2020-12-24 13:47:44 +0800

    增加登录界面，增加管理员登录

--- mgr/urls.py
+++ mgr/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from mgr import customer
+from mgr import sign_in_out
 
 urlpatterns = [
 
     path('customers', customer.dispatcher),
+    path('signin', sign_in_out.signin),
+    path('signout', sign_in_out.signout),
 ]
